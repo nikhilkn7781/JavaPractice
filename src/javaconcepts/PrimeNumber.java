b0a2b333bfd577337a2b17ffa4cde52a1112974d
nikhilkn7781 <105156907+nikhilkn7781@users.noreply.github.com>
2022-05-08 20:58:17 +0530

    Update PrimeNumber.java

--- src/javaconcepts/PrimeNumber.java
+++ src/javaconcepts/PrimeNumber.java
@@ -8,13 +8,13 @@ public class PrimeNumber {
         public static void main(String[] args){
             int n=20;
             for(int i=2;i<n;i++){
-                int count =0;
+                int count1 =0;
                 for(int j=2;j<i;j++){
                     if(i%j==0){
-                        count++;
+                        count1++;
                     }
                 }
-                if(count==0){
+                if(count1==0){
                     System.out.print(i+" ");
                 }
             }
